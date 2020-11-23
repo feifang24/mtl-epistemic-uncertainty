950b9e85d8623ccf569100655f594667259c9b25
Fei Fang <ffang19@stanford.edu>
2020-11-23 13:58:45 +0000

    pushing script

--- microsoft-mt-dnn/scripts/run_smoothed_uncertainty.sh
+++ microsoft-mt-dnn/scripts/run_smoothed_uncertainty.sh
@@ -1,1 +1,12 @@
-python ../run.py --train --data-dir ../../glue_data --uncertainty-based-sampling --mc-dropout-samples 8 --smooth-uncertainties 0.5
+for VAR in 1 2
+do
+    python ../run.py --train --data-dir ../../glue_data --uncertainty-based-sampling --mc-dropout-samples 8 --smooth-uncertainties 0.25
+done
+for VAR in 1 2 3
+do
+    python ../run.py --train --data-dir ../../glue_data --uncertainty-based-sampling --mc-dropout-samples 8 --smooth-uncertainties 0.125
+done
+for VAR in 1 2 3
+do
+    python ../run.py --train --data-dir ../../glue_data --uncertainty-based-sampling --mc-dropout-samples 8 --smooth-uncertainties 0.375
+done
